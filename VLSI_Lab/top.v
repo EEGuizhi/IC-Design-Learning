7516e7d764720264b6a557c9d06b6fb16615a26b
Guizhi <99305855+EEGuizhi@users.noreply.github.com>
2022-11-25 09:19:25 +0800

    Update top.v

--- VLSI_Lab/top.v
+++ VLSI_Lab/top.v
@@ -2,9 +2,9 @@
 module top (
     input clk,
     input reset,
-    input [7:0] Din,
-    input i_valid,
     input busy,
+    input i_valid,
+    input [7:0] Din,
     output [7:0] Dout,
     output reg o_valid
 );
@@ -18,7 +18,7 @@ module top (
     assign data = i_valid ? Din : 'bz;  // data 在輸入時接到Din 其他時間接到高阻抗 (inout的常見寫法)
     assign Dout = o_valid ? data : 0;  // 當o_valid為1時輸出Dout 其他時間都輸出0
 
-    Memory mem(.clk(clk), .rst(reset), .r_w(i_valid), .en(en), .data(data), .addr(addr));
+    Memory mem(.clk(clk), .rst(reset), .r_w(~i_valid), .en(en), .addr(addr), .data(data));
 
     always @(posedge clk) begin
         if(reset) begin
@@ -46,6 +46,7 @@ module top (
                         o_valid <= 0;
                     end
                     else begin
+                        o_valid <= 1;
                         addr <= addr - 1;
                     end
                 end
